43ee1e9992333b182ed2dd7e001bdf15f5b39124
Mohan Gc <mohangc083@gmail.com>
2025-06-26 14:43:48 +0545

    User model Updated

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -24,7 +24,7 @@ const Profile = () => {
           <div className="flex items-center gap-4">
             <Avatar className="h-24 w-24">
               <AvatarImage
-                src={user?.profile?.image}
+                src={user?.profile?.profilePhoto}
                 alt="profile"
               />
             </Avatar>
